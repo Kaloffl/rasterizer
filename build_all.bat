57747d59f2d0e0aa3fe020073a54172ca3299860
Lars Donner <lars.donner@gmail.com>
2017-07-30 00:06:55 +0200

    Build scripts now create the bin folder it it doesn't exist

--- build_all.bat
+++ build_all.bat
@@ -1,2 +1,3 @@
 echo "Building all files in src/kaloffl/rtrender/"
+if not exist bin md bin
 scalac.bat -d bin src/kaloffl/rtrender/*
--- build_single.bat
+++ build_single.bat
@@ -1,2 +1,3 @@
 echo "Building file %1"
+if not exist bin md bin
 scalac.bat -classpath bin -d bin %1
