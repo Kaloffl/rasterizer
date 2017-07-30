2f887ac980fe84d1549593355943d212376939bf
Lars Donner <lars.donner@gmail.com>
2017-07-30 22:24:24 +0200

    Textures are now stored in a z-curve pattern to make them more cache-friendly

--- src/kaloffl/rtrender/Texture.scala
+++ src/kaloffl/rtrender/Texture.scala
@@ -3,11 +3,33 @@ package kaloffl.rtrender
 class Texture(buffer: Array[Int], width: Int, height: Int) {
 
   val color_buffer: Array[Color] = new Array[Color](width * height)
-  for (xy <- 0 until height * width) {
-    color_buffer(xy) = Color.from_srgb(buffer(xy))
+  for (y <- 0 until height; x <- 0 until width) {
+    val xy = Texture.xy_to_z_curve(x, y, width)
+    color_buffer(xy) = Color.from_srgb(buffer(x + y * width))
   }
 
-  def read_nearest(x: Int, y: Int): Color = color_buffer(x + y * width)
+  def read_nearest(x: Int, y: Int): Color = color_buffer(Texture.xy_to_z_curve(x, y, width))
 
-  def read_nearest(x: Float, y: Float): Color = color_buffer((x * width).toInt + (y * height).toInt * width)
+  def read_nearest(x: Float, y: Float): Color = color_buffer(Texture.xy_to_z_curve((x * width).toInt, (y * height).toInt, width))
+}
+
+object Texture {
+    def xy_to_z_curve(x: Int, y: Int, size: Int): Int = {
+        var nx = x
+        var ny = y
+        var nsize = size
+        var index = 0
+        while (1 < nsize) {
+            nsize = nsize / 2
+            if (ny >= nsize) {
+                ny -= nsize
+                index += nsize * nsize
+            }
+            if (nx >= nsize) {
+                nx -= nsize
+                index += nsize * nsize * 2
+            }
+        }
+        return index
+    }
 }
